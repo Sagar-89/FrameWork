4b58a32b03197321003deb56bbfec34baf639ad9
Amar M <Amar M@DESKTOP-46JBKBL>
2023-07-04 22:34:05 +0530

    Build B02

--- HybridFramework/src/generic_Script/Select_and_Actions_Generic.java
+++ HybridFramework/src/generic_Script/Select_and_Actions_Generic.java
@@ -1,15 +1,19 @@
 package generic_Script;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public final class Select_and_Actions_Generic 
-{
-	Select s;
-	
-	public static void dDP()
+{	
+	public static void dDP(WebElement ele)
 	{
-
-
+		Select se = new Select(ele);
+	}
+	public static void atn(WebDriver driver)
+	{
+		Actions act = new Actions(driver);
 	}
 	
 	
